c966f1f0abe0f5f91333a824ad78788a4b8170e7
yxiaomin <1179719147@qq.com>
2017-03-01 15:21:05 +0800

    newpay

---
+++ FindHuoFFFFFFFFFF/FindHuo/app/src/main/java/com/jinyuankeji/yxm/findhuo/lottery/detail/LotteryAddPayBean.java
@@ -0,0 +1,42 @@
+package com.jinyuankeji.yxm.findhuo.lottery.detail;
+
+/**
+ * Created by  yxiaomin on 2017/3/1 0001.
+ */
+
+public class LotteryAddPayBean {
+
+    /**
+     * res : 10001
+     * cache_update : 10000
+     * data : 41
+     */
+
+    private int res;
+    private int cache_update;
+    private int data;
+
+    public int getRes() {
+        return res;
+    }
+
+    public void setRes(int res) {
+        this.res = res;
+    }
+
+    public int getCache_update() {
+        return cache_update;
+    }
+
+    public void setCache_update(int cache_update) {
+        this.cache_update = cache_update;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
